6cdc1e7030f4027c459391ba0942f7e55a8052df
christian barillas <christianbarillasyt@gmail.com>
2023-04-24 21:59:24 -0600

    updating exercise

--- ejercicio1.php
+++ ejercicio1.php
@@ -2,28 +2,15 @@
 
 class Employee{
 
-    public function saveemployee (){
-
-        return "the employee's informtion has een saved successfully";
-
+        public function saveEmployee(){
+            return "The employee's information has been saved successfully.";
+        }
+        public function connection(){
+            return " The connection to the database was successful";
+        }
+        public function reportByDepartment($department){
+            return "Information of all active employees in $department";
+        }c
+    
     }
-
-    public function connection (){
-
-    }
-
-
-
-public function reportByDepartment () {
-
-}
-
-
-
-
-
-
-
-}
-
 ?>
